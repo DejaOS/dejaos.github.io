f8de128cb7f86f0d52198b473564f98533bacf97
buter <voxer@126.com>
2026-01-21 11:15:08 +0800

    update：/zh

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/docs/welcome/" />;
+  const redirectUrl = useBaseUrl('/docs/welcome/');
+  return <Redirect to={redirectUrl} />;
 }
